649129c1c956e255a357a20ca91b914a05c1dd8f
dannykim1837 <eoghks1837@gmail.com>
2024-01-13 10:43:42 -0700

    2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
+        var first = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+        var last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
