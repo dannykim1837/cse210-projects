401e678aa511d1019f72c2003ef9722fad3737b6
dannykim1837 <eoghks1837@gmail.com>
2024-01-23 21:40:46 -0700

    learning2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello World!");
+
+
+        var job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Micro Soft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        var job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2010;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Danny Kim";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
     }
 }
+
