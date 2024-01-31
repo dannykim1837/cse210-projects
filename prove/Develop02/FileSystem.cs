b7e29a34523d532cf06d476038fa1d163c5e0003
dannykim1837 <eoghks1837@gmail.com>
2024-01-30 19:55:57 -0700

    22

--- prove/Develop02/FileSystem.cs
+++ prove/Develop02/FileSystem.cs
@@ -13,29 +13,6 @@ public class FileSystem
     public static Journal LoadFromFile(string fileName)
     {
         Journal loadedJournal = new Journal();
-
-        using (StreamReader reader = new StreamReader(fileName))
-        {
-            while (!reader.EndOfStream)
-            {
-                string line = reader.ReadLine();
-                string[] parts = line.Split(',');
-
-                if (parts.Length == 3 &&
-                    DateTime.TryParse(parts[0], out DateTime date))
-                {
-                    Entry entry = new Entry
-                    {
-                        Date = date,
-                        Prompt = parts[1],
-                        Response = parts[2]
-                    };
-
-                    loadedJournal.AddEntry(entry);
-                }
-            }
-        }
-
         return loadedJournal;
     }
     }
