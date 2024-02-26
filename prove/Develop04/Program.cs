5dfa6c7ff00bf34f1e39afe9c2437c1a1cf9b0fc
dannykim1837 <eoghks1837@gmail.com>
2024-02-26 11:07:23 -0700

    first

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool keeptrue = ture;
+        int selection = Menu();
+        while(keeptrue)
+        {
+            if(selection == 1)
+                {
+
+                }
+            else if (selection == 2)
+                {
+                    
+                }
+            else if (selection == 3)
+                {
+                    
+                }
+            else if (selection == 4)
+                {
+                    keeptrue = false;
+                    Console.WriteLine("Goodbye!");
+                }
+        }
     }
+
+    static int Menu()
+        {
+            Console.WriteLine("Menu Options \n 1. Start breathing activity \n 2. Start reflecting activity \n 3. Start listing activity \n 4. Quit");
+            Console.Write("\nSelect a choice from the menu: ");
+            var selection = int.Parse(Console.ReadLine());
+            return selection;
+        }
 }
