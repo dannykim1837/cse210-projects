b13aabf00ded7eff438293a3f5232de557ab1cd1
dannykim1837 <eoghks1837@gmail.com>
2024-03-27 18:51:32 -0600

    222

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -8,7 +8,7 @@ class Program
         {
         new Video("DJ Okawari - Flower Dance", "Bob42jh", 263),
         new Video("Joe Hisaishi - The Rain", "animefuefuki", 337),
-        new Video("Kiss the rain - Yiruma", "Lulu", 253)
+        new Video("Kiss the rain - Yiruma", "Lulu", 253),
         new Video("Jon Schmidt & Steven Sharp Nelson - Love Story Meets Viva la Vida", "Vincent Fox", 484)
         };
 
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,10 +1,27 @@
 class Video
 {
+    public string title;
+    public string authou;
+    public int length;
+    
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        comments = new List<Comment>();
+    }
+
+    public int GetNumberOfComments()
+    {
+        return comments.Count;
+    }
+
     public void Displayinfo()
     {
         System.Console.WriteLine($"Title : {title}");
         System.Console.WriteLine($"Author : {author}");
-        System.Console.WriteLine($"Duration : {duration} seconds");
-        System.Console.WriteLine($"Comments : {GetComments()}");
+        System.Console.WriteLine($"Duration : {length} seconds");
+        System.Console.WriteLine($"Comments : {GetNumberOfComments()()}");
     }
 }
